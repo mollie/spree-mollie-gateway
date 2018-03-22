c3d5d721acb806269a65ef47e091b1b42ec22918
Vernon de Goede <vernon@mollie.com>
2018-03-22 09:31:52 +0100

    Test Mollie payment creation

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -21,6 +21,33 @@ RSpec.describe Spree::Order, type: :model do
     it 'should set order state to complete' do
       expect(order.state).to eq 'complete'
     end
+
+    it 'should set a payment_url' do
+      expect(payment.source.payment_url).to include 'https://www.mollie.com/paymentscreen/'
+    end
+
+    it 'should delegate transaction_id to its source' do
+      expect(payment.transaction_id).to eq payment.source.transaction_id
+      expect(payment.transaction_id).to eq payment.source.payment_id
+    end
+  end
+
+  describe 'checkout steps' do
+    it 'should not include confirmation step by default' do
+      expect(order.checkout_steps).not_to include 'confirm'
+    end
+
+    it 'should include payment step by default' do
+      expect(order.checkout_steps).to include 'payment'
+    end
+
+    context 'with braintree dropin payment' do
+      before {add_payment_to_order!}
+
+      it 'should include payment step' do
+        expect(order.checkout_steps).to include 'payment'
+      end
+    end
   end
 
 end
