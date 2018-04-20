bc00fb6bfc457da0532ddf363afdf8c3e6fd0ec1
Vernon de Goede <vernon@mollie.com>
2018-04-20 15:32:19 +0200

    Fix typo.

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,7 +2,7 @@ Spree.user_class.class_eval do
   after_create :create_mollie_customer
 
   def create_mollie_customer
-    # Don't create Mollie customers is spree_auth_devise is not installed.
+    # Don't create Mollie customers if spree_auth_devise is not installed.
     return unless defined? Spree::User
     mollie_gateway = Spree::PaymentMethod.find_by_type 'Spree::Gateway::MollieGateway'
     mollie_customer = mollie_gateway.create_customer(self)
