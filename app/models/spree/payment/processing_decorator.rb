bb7cbbb44167f53025e81ad0d51dd3b30695cd2b
Bertoldi <bertoldi.giacomo@gmail.com>
2018-06-08 17:54:47 +0200

    fix undefined method `create_transaction' for check payment method

--- app/models/spree/payment/processing_decorator.rb
+++ app/models/spree/payment/processing_decorator.rb
@@ -1,5 +1,23 @@
 Spree::Payment::Processing.module_eval do
   def process!(amount = nil)
+    if payment_method.is_a? Spree::Gateway::MollieGateway
+      process_with_mollie
+    else
+      process_with_spree
+    end
+  end
+
+  private
+
+  def process_with_spree
+    if payment_method && payment_method.auto_capture?
+      purchase!
+    else
+      authorize!
+    end
+  end
+
+  def process_with_mollie
     amount ||= money.money.cents
     started_processing!
     response = payment_method.create_transaction(
