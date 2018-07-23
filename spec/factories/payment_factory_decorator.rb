151ee279acbf555ce428473f7050b8e81576c08f
Vernon de Goede <vernon@mollie.com>
2018-07-23 17:07:04 +0200

    Remove obsolete factory.

--- spec/factories/payment_factory_decorator.rb
+++ spec/factories/payment_factory_decorator.rb
@@ -6,12 +6,4 @@ FactoryBot.define do
     order
     state 'checkout'
   end
-
-  factory :mollie_multicurrency_payment, class: Spree::Payment do
-    amount 12.73
-    association(:payment_method, factory: :mollie_gateway)
-    association(:source, factory: :mollie_ideal_payment_source)
-    order
-    state 'checkout'
-  end
 end
