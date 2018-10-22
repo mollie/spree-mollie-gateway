c1789d89feaacc8038a7bc0bfe63d43d5c5eb7e5
Vernon de Goede <vernon@mollie.com>
2018-10-22 11:24:39 +0200

    Payment refunds should not update order.

--- spec/factories/payment_factory_decorator.rb
+++ spec/factories/payment_factory_decorator.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :mollie_payment, class: Spree::Payment do
     amount 12.73
     association(:payment_method, factory: :mollie_gateway)
-    association(:source, factory: :mollie_cc_payment_source)
+    association(:source, factory: :mollie_payment_source)
     order
     state 'checkout'
   end
