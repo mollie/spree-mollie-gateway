c5f788662848703f7a1efaadfe20f034634aecc7
Fabian Oudhaarlem <fabian@coqtail.com>
2019-02-01 16:22:05 +0100

    Only reset the order to state payment when the order is not paid.

--- app/models/spree/mollie/payment_state_updater.rb
+++ app/models/spree/mollie/payment_state_updater.rb
@@ -28,7 +28,7 @@ module Spree
           @spree_payment.source.update(status: @spree_payment.state)
         else
           MollieLogger.debug("Unhandled Mollie payment state received: #{@mollie_order.status}. Therefore we did not update the payment state.")
-          @spree_payment.order.update_attributes(state: 'payment', completed_at: nil)
+          @spree_payment.order.update_attributes(state: 'payment', completed_at: nil) unless @spree_payment.order.paid?
         end
       end
 
