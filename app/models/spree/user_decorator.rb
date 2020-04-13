3796b8f698e670ac1459ccb63104ad3592dc7ea4
Rene van Lieshout <rene@lico.nl>
2020-04-13 10:57:12 +0200

    Don't create a Mollie customer when the gateway is inactive
    A client had invalid credentials in the Gateway settings which resulted in a Mollie::RequestError. Let's only create a Mollie customer when the gateway is active.
    
    Using an ensure_mollie_customer after_save should also create Mollie customers for users that have been created in the time that is was inactive

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,16 +1,19 @@
 module Spree::UserDecorator
 
   def self.prepended(base)
-    base.after_create :create_mollie_customer
+    base.after_save :ensure_mollie_customer
   end
 
 
-  def create_mollie_customer
+  def ensure_mollie_customer
+    # Don't create a Mollie customer if it has been created before
+    return if mollie_customer_id.present?
+
     # Don't create Mollie customers if spree_auth_devise is not installed.
     return unless defined? Spree::User
 
     mollie_gateway = Spree::PaymentMethod.find_by_type 'Spree::Gateway::MollieGateway'
-    return unless mollie_gateway.present?
+    return unless mollie_gateway.present? && mollie_gateway.active?
 
     mollie_customer = mollie_gateway.create_customer(self)
     update mollie_customer_id: mollie_customer.id
