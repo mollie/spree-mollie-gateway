674be36d9c076af1c7b85511c993494f4a9b6781
Rene van Lieshout <rene@lico.nl>
2020-08-10 07:44:44 +0200

    Use after_commit to ensure mollie customer and use a safe operator

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,19 +1,18 @@
 module Spree::UserDecorator
 
   def self.prepended(base)
-    base.after_save :ensure_mollie_customer
+    base.after_commit :ensure_mollie_customer
   end
 
 
   def ensure_mollie_customer
-    # Don't create a Mollie customer if it has been created before
     return if mollie_customer_id.present?
 
     # Don't create Mollie customers if spree_auth_devise is not installed.
     return unless defined? Spree::User
 
     mollie_gateway = Spree::PaymentMethod.find_by_type 'Spree::Gateway::MollieGateway'
-    return unless mollie_gateway.present? && mollie_gateway.active?
+    return unless mollie_gateway&.active?
 
     mollie_customer = mollie_gateway.create_customer(self)
     update mollie_customer_id: mollie_customer.id
