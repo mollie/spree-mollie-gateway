78a7fc10fa1b3f11cf66e648a0698bc08782d981
Rene van Lieshout <rene@lico.nl>
2020-08-10 10:26:13 +0200

    mollie_customer_id could not exist

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,15 +4,14 @@ module Spree::UserDecorator
     base.after_commit :ensure_mollie_customer, on: %i[create update]
   end
 
-
   def ensure_mollie_customer
+    return if mollie_customer_id&.present?
+
     # Don't create Mollie customers if spree_auth_devise is not installed.
     return unless defined? Spree::User
 
     mollie_gateway = Spree::PaymentMethod.find_by_type 'Spree::Gateway::MollieGateway'
     return unless mollie_gateway&.active?
-
-    return if mollie_customer_id.present?
 
     mollie_customer = mollie_gateway.create_customer(self)
     update mollie_customer_id: mollie_customer.id
